d12c789a110d4e071463a915eef5e755b2c563be
Vigo Matuka <vigo.matuka@gmail.com>
2025-11-25 14:53:17 +0200

    Added service layer

--- BookstoreAPI/src/main/java/com/example/bookstore/service/AuthorService.java
+++ BookstoreAPI/src/main/java/com/example/bookstore/service/AuthorService.java
@@ -1,5 +1,29 @@
 package com.example.bookstore.service;
+import com.example.bookstore.entity.Author;
+import com.example.bookstore.repository.AuthorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
 
+@Service
 public class AuthorService {
-    
+    @Autowired
+    private AuthorRepository authorRepository;
+
+    public List<Author> getAllAuthors(){
+        return authorRepository.findAll();
+    }
+    public Optional<Author> getAuthorById(Long id){
+        return authorRepository.findById(id);
+    }
+    public Author saveAuthor(Author author){
+        return authorRepository.save(author);
+    }
+    public void deleteAuthor(Long id){
+        authorRepository.deleteById(id);
+    }
+    public List<Author> searchAuthorsByName(String name){
+        return authorRepository.findByNameContaining(name);
+    }
 }
